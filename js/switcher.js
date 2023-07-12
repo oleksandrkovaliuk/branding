ad23f48050d73b216e9e0ba19ab0604a2d358386
sasha <sasha1235qaz@ukr.net>
2023-07-11 23:08:46 -0700

    all pages adaptived until 320px

--- js/switcher.js
+++ js/switcher.js
@@ -8,21 +8,21 @@ const showBlockDown = document.querySelector('#show-section');
 const blockDown = document.querySelector('#show-info-block');
 const closeShowblockBtn = document.querySelector('#close-show-block');
 
-monthBtn.addEventListener('click' , () => {
+monthBtn.addEventListener('click', () => {
     monthBtn.classList.add('show');
     annualBtn.classList.remove('show');
     liteBox.querySelector('.money').innerHTML = "17";
     growthBox.querySelector('.money').innerHTML = "53";
     proBox.querySelector('.money').innerHTML = "125"
 })
-annualBtn.addEventListener('click' , () => {
+annualBtn.addEventListener('click', () => {
     monthBtn.classList.remove('show');
     annualBtn.classList.add('show');
     liteBox.querySelector('.money').innerHTML = "53";
     growthBox.querySelector('.money').innerHTML = "115";
     proBox.querySelector('.money').innerHTML = "250";
 })
-showBlockDown.addEventListener('click' , () => {
+showBlockDown.addEventListener('click', () => {
     blockDown.classList.toggle('show');
     closeShowblockBtn.classList.toggle('show');
 })
